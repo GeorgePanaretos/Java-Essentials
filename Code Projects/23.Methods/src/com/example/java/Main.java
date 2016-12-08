3f8d6c34f54bfa82026e612131d1bcdf6f996645
George Panaretos <georgepanaretos.cac@gmail.com>
2016-12-08 01:56:59 +0100

    Methods

---
+++ Code Projects/23.Methods/src/com/example/java/Main.java
@@ -0,0 +1,27 @@
+package com.example.java;
+
+public class Main {
+
+    static String[] months = {"January", "February", "March",
+            "April", "May", "June",
+            "July", "August", "September",
+            "October", "November", "December"};
+
+    public static void main(String[] args) {
+        loopIt("Months of the year");
+        loopIt("Second Run");
+        loopIt("Third Run");
+
+    }
+    public static void loopIt(String label){
+        System.out.println(label);
+        for (int i = 0; i <label.length() ; i++) {
+            System.out.print("*");
+        }
+        System.out.println("");
+        for (int i = 0; i <months.length ; i++) {
+            System.out.println(months[i]);
+        }
+    }
+
+}
