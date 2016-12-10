e9168878e6426f968d9000c96e1ec3fa65aed3cb
George Panaretos <georgepanaretos.cac@gmail.com>
2016-12-11 00:20:22 +0100

    Extending Classes

---
+++ Code Projects/38.ExtendingClasses/src/com/example/java/model/Kalamata.java
@@ -0,0 +1,12 @@
+package com.example.java.model;
+
+/**
+ * Created by george156200911 on 2/12/2016.
+ */
+public class Kalamata extends Olive {
+
+    public Kalamata(){
+        super(OliveName.KALAMATA,OliveColor.PURPLE,2);
+
+    }
+}
---
+++ Code Projects/38.ExtendingClasses/src/com/example/java/model/Ligurian.java
@@ -0,0 +1,12 @@
+package com.example.java.model;
+
+/**
+ * Created by george156200911 on 2/12/2016.
+ */
+public class Ligurian extends Olive {
+
+    public Ligurian(){
+        super(OliveName.LIGURIAN,OliveColor.BLACK,5);
+
+    }
+}
