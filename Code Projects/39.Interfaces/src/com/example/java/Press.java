4211d0ccefb1cb67c844bb84d4ec49f4d390c16a
George Panaretos <georgepanaretos.cac@gmail.com>
2016-12-11 00:20:41 +0100

    Inteface

---
+++ Code Projects/39.Interfaces/src/com/example/java/Press.java
@@ -0,0 +1,15 @@
+package com.example.java;
+
+import com.example.java.model.Olive;
+
+import java.util.List;
+
+/**
+ * Created by george156200911 on 2/12/2016.
+ */
+public interface Press {
+
+    public int getOil(List<Olive> olives);
+    public void setOil(int oil);
+
+    }
